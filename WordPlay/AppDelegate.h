1b2124607670cd5e7095ea4a3ca24af2c7edb804
irwingonzales92 <irwin@newdesto.com>
2015-01-06 15:39:25 -0800

    Initial Commit

---
+++ WordPlay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WordPlay
+//
+//  Created by Irwin Gonzales on 1/6/15.
+//  Copyright (c) 2015 Irwin Gonzales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
